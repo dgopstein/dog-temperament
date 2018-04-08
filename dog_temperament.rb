9cf7c2f859d5faa4db6ed98d2b1eb0e7ec9a9369
Dan Gopstein <dan@gopstein.com>
2018-04-07 23:41:16 -0400

    boxplots

--- dog_temperament.rb
+++ dog_temperament.rb
@@ -12,7 +12,7 @@ noko_htmls = htmls.map{|html| Nokogiri::HTML(html)}
 
 rows = noko_htmls.flat_map{|noko_html| noko_html.css('tr').drop(1)}
 
-raw_vals = rows.map{|row| row.css('td span strong').map{|st| st.text}}[0...-1]
+raw_vals = rows.map{|row| row.css('td').map{|st| st.text}}[0...-1]
 
 vals = raw_vals.map do |dog, tested, passes, fails, percent|
   [dog.split.map(&:capitalize).join(' '), tested.to_i, passes.to_i, fails.to_i]
